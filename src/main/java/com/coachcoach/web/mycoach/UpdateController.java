7f4452f40a9b246e7b1762f5a752d7dbf76cd7f3
eunjeong <euuun22@naver.com>
2020-04-20 20:07:40 +0900

    [add] withdrawForm(비밀번호 재확인) jsp 추가

--- src/main/java/com/coachcoach/web/mycoach/UpdateController.java
+++ src/main/java/com/coachcoach/web/mycoach/UpdateController.java
@@ -25,9 +25,15 @@ public class UpdateController {
   @PostMapping("update")
   public void update() {}
 
-  @GetMapping("withdrawForm")
-  public void withdrawForm() {}
+  @GetMapping("withdrawForm1")
+  public void withdrawForm1() {}
 
-  @GetMapping("withdraw")
-  public void withdraw() {} // refresh로 jsp 출력후 메인 페이지 이동
+  @GetMapping("withdraw1")
+  public void withdraw1() {}
+
+  @GetMapping("withdrawForm2")
+  public void withdrawForm2() {}
+
+  @GetMapping("withdraw2")
+  public void withdraw2() {} // refresh로 jsp 출력후 메인 페이지 이동
 }
