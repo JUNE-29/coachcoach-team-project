bb881516363f61fcdb0b92bc7db97c52ec7eccd1
ekgp49 <ekgp4949@gmail.com>
2020-04-23 15:10:33 +0900

    [add] FoodBoardService 생성]

--- src/main/java/com/coachcoach/dao/FoodBoardDao.java
+++ src/main/java/com/coachcoach/dao/FoodBoardDao.java
@@ -8,14 +8,14 @@ public interface FoodBoardDao {
   int insert(FoodBoard foodBoard) throws Exception;
   // foodBoard 등록 시 사용
 
-  List<FoodBoard> findByMemberNo(int memberNo) throws Exception;
+  List<FoodBoard> findAll(int memberNo) throws Exception;
   // 회원이 로그인 하고 본인의 foodBoard 게시글만 보여야 해서
   // memberNo가 필요한것같아 파라미터로 넣었습니다
 
   FoodBoard findByNo(int no) throws Exception;
   // 게시글 클릭시 게시글의 디테일을 보여주기 위함
 
-  int update(FoodBoard FoodBoard) throws Exception;
+  int update(FoodBoard foodBoard) throws Exception;
   // 게시글 업데이트시 사용
 
   int delete(int no) throws Exception;
---
+++ src/main/java/com/coachcoach/service/impl/FoodBoardServiceImpl.java
@@ -0,0 +1,43 @@
+package com.coachcoach.service.impl;
+
+import java.util.List;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import com.coachcoach.dao.FoodBoardCommentDao;
+import com.coachcoach.dao.FoodBoardDao;
+import com.coachcoach.domain.FoodBoard;
+import com.coachcoach.service.FoodBoardService;
+
+@Component
+public class FoodBoardServiceImpl implements FoodBoardService {
+  FoodBoardDao foodBoardDao;
+  FoodBoardCommentDao foodBoardCommentDao;
+
+  @Override
+  public int add(FoodBoard foodBoard) throws Exception {
+    return foodBoardDao.insert(foodBoard);
+  }
+
+  @Override
+  public List<FoodBoard> list(int memberNo) throws Exception {
+    return foodBoardDao.findAll(memberNo);
+  }
+
+  @Override
+  public FoodBoard get(int no) throws Exception {
+    return foodBoardDao.findByNo(no);
+  }
+
+  @Override
+  public int update(FoodBoard foodBoard) throws Exception {
+    return foodBoardDao.update(foodBoard);
+  }
+
+  @Transactional
+  @Override
+  public int delete(int no) throws Exception {
+    foodBoardCommentDao.deleteAll(no);
+    return foodBoardDao.delete(no);
+  }
+
+}
