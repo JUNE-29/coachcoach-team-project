c9874838136b7300cae4006aa07f3c8ee1c94be3
ekgp49 <ekgp4949@gmail.com>
2020-04-28 13:27:29 +0900

    [mod] 폴더 이름 수정 및 코치페이지 요청리스트 수정

--- src/main/java/com/coachcoach/web/coachPage/RequestReceivedController.java
+++ src/main/java/com/coachcoach/web/coachPage/RequestReceivedController.java
@@ -3,35 +3,40 @@ package com.coachcoach.web.coachPage;
 import javax.servlet.ServletContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.coachcoach.service.CoachService;
+import com.coachcoach.service.MemberCoachingProgramService;
 
 @Controller
-@RequestMapping("/coachpage/requestReceived")
+@RequestMapping("/coachPage/requestReceived")
 public class RequestReceivedController {
 
   @Autowired
   ServletContext servletContext;
 
   @Autowired
-  CoachService coachService;
+  MemberCoachingProgramService memberCoachingProgramService;
 
   @GetMapping("list")
-  public void list() {}
+  public void list(int coachNo, Model model) throws Exception {
+    model.addAttribute("list", memberCoachingProgramService.RequestList(coachNo));
+  }
 
-  @GetMapping("detail")
-  public void detail() {}
+  @GetMapping("detail") // memberCoachingProgramNo 받아서 요청서 보는 데에 씀
+  public void detail(int no, Model model) throws Exception {
+    model.addAttribute("detail", memberCoachingProgramService.get(no));
+  }
 
   @PostMapping("reject")
-  public void reject() {}
+  public void reject() throws Exception {}
 
   @GetMapping("rejectForm")
-  public void rejectForm() {}
+  public void rejectForm() throws Exception {}
 
   @GetMapping("accept")
-  public void accept() {}
+  public void accept() throws Exception {}
 
 
 }
