d64274b4ee46955775eae3139a8b98a0f8846234
ekgp49 <ekgp4949@gmail.com>
2020-05-18 10:53:10 +0900

    [del] 잘못생성한 Node폴더 삭제

--- src/main/webapp/resources_k/js/main.js
+++ src/main/webapp/resources_k/js/main.js
@@ -478,8 +478,8 @@
     }
   })
   
-  $('#addNoticeBoard .board-content').keyup(function() {
-    if($("#addNoticeBoard .board-content").val()){
+  $('#addNoticeBoard .summernote').keyup(function() {
+    if($("#addNoticeBoard .summernote").summernote('code')){
       $("#addNoticeBoard .content-box p").remove();
     }
   })
