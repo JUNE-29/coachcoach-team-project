a7769a09c96fbe42ad6af6a1450b79216f6bce77
ekgp49 <ekgp4949@gmail.com>
2020-05-19 11:51:02 +0900

    [mod] 캘린더 적용

--- src/main/webapp/resources_k/js/main2.js
+++ src/main/webapp/resources_k/js/main2.js
@@ -3,7 +3,19 @@ document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    plugins: [ 'dayGrid' ]
+    plugins: ['interaction', 'dayGrid'],
+    selectable: true,
+    header: {
+      left: '',
+      center: 'title',
+    },
+    locale: 'ko',
+    dateClick: function(info) {
+      alert('clicked ' + info.dateStr);
+    },
+    select: function(info) {
+      alert('selected ' + info.startStr + ' to ' + info.endStr);
+    }
   });
 
   calendar.render();
