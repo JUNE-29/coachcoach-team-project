850791aeb5f1ed9b3835a3cfc97b6723d4325b38
rogihyun <srlgus2002v@naver.com>
2020-05-25 14:53:22 +0900

    [mod]페이징

--- src/main/java/com/coachcoach/dao/NoticeBoardDao.java
+++ src/main/java/com/coachcoach/dao/NoticeBoardDao.java
@@ -2,17 +2,24 @@ package com.coachcoach.dao;
 
 import java.util.List;
 import com.coachcoach.domain.NoticeBoard;
+import com.coachcoach.domain.Pagination;
 
 public interface NoticeBoardDao {
   public int insert(NoticeBoard noticeboard) throws Exception;
 
-  public List<NoticeBoard> findAll() throws Exception;
+  public List<NoticeBoard> findAll(Pagination pagination) throws Exception;
 
   public NoticeBoard findByNo(int no) throws Exception;
 
   public int update(NoticeBoard noticeboard) throws Exception;
 
   public int delete(int no) throws Exception;
+
+//총 게시글 개수 확인
+
+public int getnoticeBoardListCnt() throws Exception;
+
+
 }
 
 
