8815e99d309ad353cf329b8e62b69935de418f27
dahye <ekgp4949@gmail.com>
2020-06-01 18:40:31 +0900

    [mod] 캘린더 수정중

--- src/main/java/com/coachcoach/web/myPage/CoachingController.java
+++ src/main/java/com/coachcoach/web/myPage/CoachingController.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.tika.Tika;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import com.coachcoach.domain.CalendarFile;
 import com.coachcoach.domain.Member;
@@ -31,6 +33,7 @@ import com.coachcoach.service.CoachingProgramBoardService;
 import com.coachcoach.service.MemberCoachingProgramService;
 import com.coachcoach.service.MemberProgramCalendarService;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 @Auth(role = Role.COACH)
 @Controller
@@ -148,6 +151,34 @@ public class CoachingController {
   @PostMapping("updateForm") // 운동 코칭 수정
   public void updateForm(Model model, int no) throws Exception {
     model.addAttribute("detail", memberProgramCalendarService.get(no));
+  }
+
+  @ResponseBody
+  @PostMapping(value="uploadSummernoteImageFile", produces = "application/json")
+  public JsonObject uploadSummernoteImageFile(@RequestParam("file") MultipartFile multipartFile) throws Exception {
+    JsonObject jsonObject = new JsonObject();
+
+    String dirPath = servletContext.getRealPath("/upload/calendarFile");
+    String originalFileName = multipartFile.getOriginalFilename();  //오리지날 파일명
+    String extension = originalFileName.substring(originalFileName.lastIndexOf("."));   //파일 확장자
+
+    String savedFileName = UUID.randomUUID() + extension;   //저장될 파일 명
+
+    File targetFile = new File(dirPath + savedFileName);
+
+    try {
+        InputStream fileStream = multipartFile.getInputStream();
+        FileUtils.copyInputStreamToFile(fileStream, targetFile);    //파일 저장
+        jsonObject.addProperty("url", dirPath+"/"+savedFileName);
+        jsonObject.addProperty("responseCode", "success");
+
+    } catch (IOException e) {
+        FileUtils.deleteQuietly(targetFile);    //저장된 파일 삭제
+        jsonObject.addProperty("responseCode", "error");
+        e.printStackTrace();
+    }
+
+    return jsonObject;
   }
 
   @PostMapping("update")
